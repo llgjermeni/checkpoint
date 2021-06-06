684c29d08fd90b0a8fa50c48804b8794ecbf6336
llgjermeni <laertgjermeni@hotmail.com>
2021-06-06 21:46:12 +0200

    change

--- vmss-existing-vnet/main.tf
+++ vmss-existing-vnet/main.tf
@@ -1,44 +1,3 @@
-terraform {
-  required_version = ">= 0.14.3"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.17.0"
-    }
-    random = {
-      version = "~> 2.2.1"
-    }
-  }
-}
-
-provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
-
-  features {}
-}
-
-//********************** Basic Configuration **************************//
-module "common" {
-  source = "../modules/common"
-  resource_group_name = var.resource_group_name
-  location = var.location
-  admin_password = var.admin_password
-  installation_type = var.installation_type
-  template_name = var.template_name
-  template_version = var.template_version
-  number_of_vm_instances = var.number_of_vm_instances
-  allow_upload_download = var.allow_upload_download
-  vm_size = var.vm_size
-  disk_size = var.disk_size
-  is_blink = var.is_blink
-  os_version = var.os_version
-  vm_os_sku = var.vm_os_sku
-  vm_os_offer = var.vm_os_offer
-  authentication_type = var.authentication_type
-}
 
 //********************** Networking **************************//
 
@@ -57,16 +16,16 @@ data "azurerm_subnet" "backend" {
 //********************** Load Balancers **************************//
 resource "azurerm_public_ip" "public-ip-lb" {
     name = "${var.vmss_name}-app-1"
-    location = module.common.resource_group_location
-    resource_group_name = module.common.resource_group_name
+    location = var.location
+    resource_group_name = var.resource_group_name
     allocation_method = var.vnet_allocation_method
     sku = var.sku
 }
 
 resource "azurerm_lb" "frontend-lb" {
  name = "frontend-lb"
- location = module.common.resource_group_location
- resource_group_name = module.common.resource_group_name
+ location = var.location
+ resource_group_name = var.resource_group_name
  sku = var.sku
 
  frontend_ip_configuration {
@@ -76,15 +35,15 @@ resource "azurerm_lb" "frontend-lb" {
 }
 
 resource "azurerm_lb_backend_address_pool" "frontend-lb-pool" {
- resource_group_name = module.common.resource_group_name
  loadbalancer_id = azurerm_lb.frontend-lb.id
  name = "${var.vmss_name}-app-1"
+ resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_lb" "backend-lb" {
  name = "backend-lb"
- location = module.common.resource_group_location
- resource_group_name = module.common.resource_group_name
+ location = var.location
+ resource_group_name = var.resource_group_name
  sku = var.sku
  frontend_ip_configuration {
    name = "backend-lb"
@@ -97,13 +56,13 @@ resource "azurerm_lb" "backend-lb" {
 resource "azurerm_lb_backend_address_pool" "backend-lb-pool" {
   name = "backend-lb-pool"
   loadbalancer_id = azurerm_lb.backend-lb.id
-  resource_group_name = module.common.resource_group_name
+ resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_lb_probe" "azure_lb_healprob" {
   depends_on = [azurerm_lb.frontend-lb, azurerm_lb.frontend-lb]
   count = 2
-  resource_group_name = module.common.resource_group_name
+  resource_group_name = var.resource_group_name
   loadbalancer_id = count.index == 0 ? azurerm_lb.frontend-lb.id : azurerm_lb.backend-lb.id
   name = count.index == 0 ? "${var.vmss_name}-app-1" : "backend-lb"
   protocol = var.lb_probe_protocol
@@ -115,7 +74,7 @@ resource "azurerm_lb_probe" "azure_lb_healprob" {
 resource "azurerm_lb_rule" "lbnatrule" {
   depends_on = [azurerm_lb.frontend-lb,azurerm_lb_probe.azure_lb_healprob,azurerm_lb.backend-lb]
   count = 2
-  resource_group_name = module.common.resource_group_name
+  resource_group_name = var.resource_group_name
   loadbalancer_id = count.index == 0 ? azurerm_lb.frontend-lb.id : azurerm_lb.backend-lb.id
   name = count.index == 0 ? "${var.vmss_name}-app-1" : "backend-lb"
   protocol = count.index == 0 ? "Tcp" : "All"
@@ -132,16 +91,16 @@ resource "azurerm_lb_rule" "lbnatrule" {
 resource "random_id" "randomId" {
     keepers = {
         # Generate a new ID only when a new resource group is defined
-        resource_group = module.common.resource_group_name
+         resource_group_name = var.resource_group_name
     }
     byte_length = 8
 }
 resource "azurerm_storage_account" "vm-boot-diagnostics-storage" {
     name = "diag${random_id.randomId.hex}"
-    resource_group_name = module.common.resource_group_name
-    location = module.common.resource_group_location
-    account_tier = module.common.storage_account_tier
-    account_replication_type = module.common.account_replication_type
+    resource_group_name = var.resource_group_name
+    location = var.location
+    account_tier = var.storage_account_tier
+    account_replication_type = var.account_replication_type
 
 }
 
@@ -158,7 +117,7 @@ resource "azurerm_image" "custom-image" {
   count = local.custom_image_condition ? 1 : 0
   name = "custom-image"
   location = var.location
-  resource_group_name = module.common.resource_group_name
+  resource_group_name = var.resource_group_name
 
   os_disk {
     os_type  = "Linux"
@@ -167,10 +126,18 @@ resource "azurerm_image" "custom-image" {
   }
 }
 
+resource "azurerm_marketplace_agreement" "marketplace" {
+  publisher = var.publisher
+  offer     = var.vm_os_offer
+  plan      = var.vm_os_sku
+}
+
 resource "azurerm_virtual_machine_scale_set" "vmss" {
+  depends_on = [
+    azurerm_marketplace_agreement.marketplace]
   name = var.vmss_name
-  location = module.common.resource_group_location
-  resource_group_name = module.common.resource_group_name
+  location = var.location
+  resource_group_name = var.resource_group_name
   zones = local.availability_zones_num_condition
   overprovision = false
 
@@ -184,41 +151,41 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
 
   storage_profile_image_reference {
     id = local.custom_image_condition ? azurerm_image.custom-image[0].id : null
-    publisher = local.custom_image_condition ? null : module.common.publisher
-    offer = module.common.vm_os_offer
-    sku = module.common.vm_os_sku
-    version = module.common.vm_os_version
+    publisher = local.custom_image_condition ? null : var.publisher
+    offer = var.vm_os_offer
+    sku = var.vm_os_sku
+    version = var.vm_os_version
   }
 
   storage_profile_os_disk {
-    create_option = module.common.storage_os_disk_create_option
-    caching = module.common.storage_os_disk_caching
-    managed_disk_type = module.common.storage_account_type
+    create_option = var.storage_os_disk_create_option
+    caching = var.storage_os_disk_caching
+    managed_disk_type = var.storage_account_type
   }
 
   dynamic "plan" {
     for_each = local.custom_image_condition ? [
     ] : [1]
     content {
-      name = module.common.vm_os_sku
-      publisher = module.common.publisher
-      product = module.common.vm_os_offer
+      name = var.vm_os_sku
+      publisher = var.publisher
+      product = var.vm_os_offer
     }
   }
 
   os_profile {
     computer_name_prefix  = var.vmss_name
-    admin_username = module.common.admin_username
-    admin_password = module.common.admin_password
+    admin_username = var.admin_username
+    admin_password = var.admin_password
     custom_data = templatefile("${path.module}/cloud-init.sh",{
-      installation_type=module.common.installation_type
-      allow_upload_download= module.common.allow_upload_download
-      os_version=module.common.os_version
-      template_name=module.common.template_name
-      template_version=module.common.template_version
-      is_blink=module.common.is_blink
+      installation_type=var.installation_type
+      allow_upload_download= var.allow_upload_download
+      os_version=var.os_version
+      template_name=var.template_name
+      template_version=var.template_version
+      is_blink=var.is_blink
       bootstrap_script64=base64encode(var.bootstrap_script)
-      location=module.common.resource_group_location
+      location=var.location
       sic_key=var.sic_key
       vnet=data.azurerm_subnet.frontend.address_prefix
       enable_custom_metrics=var.enable_custom_metrics ? "yes" : "no"
@@ -239,8 +206,8 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
   }
 
   boot_diagnostics {
-    enabled = module.common.boot_diagnostics
-    storage_uri = module.common.boot_diagnostics ? join(",", azurerm_storage_account.vm-boot-diagnostics-storage.*.primary_blob_endpoint) : ""
+    enabled = var.boot_diagnostics
+    storage_uri = var.boot_diagnostics ? join(",", azurerm_storage_account.vm-boot-diagnostics-storage.*.primary_blob_endpoint) : ""
   }
 
   upgrade_policy_mode = "Manual"
@@ -272,7 +239,7 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
  }
   sku {
     capacity = var.number_of_vm_instances
-    name = module.common.vm_size
+    name = var.vm_size
     tier = "Standard"
   }
 
@@ -299,15 +266,15 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
 resource "azurerm_monitor_autoscale_setting" "vmss_settings" {
   depends_on = [azurerm_virtual_machine_scale_set.vmss]
   name = var.vmss_name
-  resource_group_name = module.common.resource_group_name
-  location = module.common.resource_group_location
+  resource_group_name = var.resource_group_name
+  location = var.location
   target_resource_id  = azurerm_virtual_machine_scale_set.vmss.id
 
   profile {
     name = "Profile1"
 
     capacity {
-      default = module.common.number_of_vm_instances
+      default = var.number_of_vm_instances
       minimum = var.minimum_number_of_vm_instances
       maximum = var.maximum_number_of_vm_instances
     }
@@ -367,7 +334,7 @@ resource "azurerm_role_assignment" "custom_metrics_role_assignment"{
   count = var.enable_custom_metrics ? 1 : 0
   role_definition_id = join("", ["/subscriptions/", var.subscription_id, "/providers/Microsoft.Authorization/roleDefinitions/", "3913510d-42f4-4e42-8a64-420c390055eb"])
   principal_id = lookup(azurerm_virtual_machine_scale_set.vmss.identity[0], "principal_id")
-  scope = module.common.resource_group_id
+  scope = var.resource_group_id
   lifecycle {
     ignore_changes = [
       role_definition_id, principal_id
--- vmss-new-vnet/main.tf
+++ vmss-new-vnet/main.tf
@@ -33,9 +33,9 @@ resource "azurerm_lb" "frontend-lb" {
 }
 
 resource "azurerm_lb_backend_address_pool" "frontend-lb-pool" {
- resource_group_name = var.resource_group_name
  loadbalancer_id = azurerm_lb.frontend-lb.id
  name = "${var.vmss_name}-app-1"
+ resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_lb" "backend-lb" {

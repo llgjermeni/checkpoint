44c07b19261f9d9304787fb3c1f3e497304a3f05
llgjermeni <laertgjermeni@hotmail.com>
2021-07-01 19:28:01 +0200

    nginx

--- nginx/main.tf
+++ nginx/main.tf
@@ -1,43 +1,57 @@
-//********************** Providers **************************//
-
-resource "azurerm_public_ip" "public-ip" {
-  name = var.mgmt_name
-  location = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method = var.vnet_allocation_method
-  idle_timeout_in_minutes = 30
-  domain_name_label = join("", [
-    var.mgmt_name,
-    "-",
-    random_id.randomId.hex])
-}
 
 resource "azurerm_subnet_network_security_group_association" "security_group_frontend_association" {
   depends_on = [var.nsg_id]
-  subnet_id = var.vnet_subnets[0]
+  subnet_id = var.vnet_subnets[1]
   network_security_group_id = var.nsg_id
 }
 
-resource "azurerm_network_interface_security_group_association" "security_group_association" {
-  depends_on = [azurerm_network_interface.nic, var.nsg_id]
-  network_interface_id = azurerm_network_interface.nic.id
+resource "azurerm_subnet_network_security_group_association" "security_group_backend_association" {
+  depends_on = [var.nsg_id]
+  subnet_id = var.subnets_id[2]
   network_security_group_id = var.nsg_id
 }
 
-resource "azurerm_network_interface" "nic" {
-  depends_on = [
-    azurerm_public_ip.public-ip]
-  name = "${var.mgmt_name}-eth0"
+resource "azurerm_network_interface_security_group_association" "security_group_association1" {
+  depends_on = [azurerm_network_interface.nic1, var.nsg_id]
+  network_interface_id = azurerm_network_interface.nic1.id
+  network_security_group_id = var.nsg_id
+}
+
+resource "azurerm_network_interface_security_group_association" "security_group_association2" {
+  depends_on = [azurerm_network_interface.nic2, var.nsg_id]
+  network_interface_id = azurerm_network_interface.nic2.id
+  network_security_group_id = var.nsg_id
+}
+
+resource "azurerm_network_interface" "nic1" {
+  # depends_on = [
+  #   azurerm_public_ip.public-ip]
+  name = "${var.nginx_name}-eth0"
   location = var.location
   resource_group_name = var.resource_group_name
   enable_ip_forwarding = false
 
   ip_configuration {
     name = "ipconfig1"
-    subnet_id = var.vnet_subnets[0]
+    subnet_id = var.vnet_subnets[1]
     private_ip_address_allocation = var.vnet_allocation_method
     private_ip_address = cidrhost(var.subnet_prefixes, 4)
-    public_ip_address_id = azurerm_public_ip.public-ip.id
+    # public_ip_address_id = azurerm_public_ip.public-ip.id
+  }
+}
+
+resource "azurerm_network_interface" "nic2" {
+  name                          = "eth1"
+  location                = var.location
+  resource_group_name     = var.resource_group_name
+  enable_ip_forwarding          = true
+  enable_accelerated_networking = true
+
+  ip_configuration {
+    name                          = "ipconfig2"
+    subnet_id                     = var.subnet_id[2]
+    private_ip_address_allocation = var.vnet_allocation_method
+    private_ip_address            = cidrhost(var.subnet_prefixes, 4)
   }
 }
 
@@ -62,21 +76,21 @@ resource "azurerm_storage_account" "vm-boot-diagnostics-storage" {
 //********************** Virtual Machines **************************//
 locals {
   SSH_authentication_type_condition = var.authentication_type == "SSH Public Key" ? true : false
-  custom_image_condition = var.source_image_vhd_uri == "noCustomUri" ? false : true
+  # custom_image_condition = var.source_image_vhd_uri == "noCustomUri" ? false : true
 }
 
-resource "azurerm_image" "custom-image" {
-  count = local.custom_image_condition ? 1 : 0
-  name = "custom-image"
-  location = var.location
-  resource_group_name = var.resource_group_name
+# resource "azurerm_image" "custom-image" {
+#   count = local.custom_image_condition ? 1 : 0
+#   name = "custom-image"
+#   location = var.location
+#   resource_group_name = var.resource_group_name
 
-  os_disk {
-    os_type  = "Linux"
-    os_state = "Generalized"
-    blob_uri = var.source_image_vhd_uri
-  }
-}
+#   os_disk {
+#     os_type  = "Linux"
+#     os_state = "Generalized"
+#     blob_uri = var.source_image_vhd_uri
+#   }
+# }
 
 resource "azurerm_marketplace_agreement" "marketplace" {
   publisher = var.publisher
@@ -84,12 +98,13 @@ resource "azurerm_marketplace_agreement" "marketplace" {
   plan      = var.vm_os_sku
 }
 
-resource "azurerm_virtual_machine" "mgmt-vm-instance" {
+resource "azurerm_virtual_machine" "nginx-vm-instance" {
   depends_on = [
-    azurerm_network_interface.nic,
+    azurerm_network_interface.nic1,
+    azurerm_network_interface.nic2,
     azurerm_marketplace_agreement.marketplace]
   location = var.location
-  name = var.mgmt_name
+  name = var.nginx_name
   network_interface_ids = [
     azurerm_network_interface.nic.id]
   resource_group_name = var.resource_group_name
@@ -101,15 +116,11 @@ resource "azurerm_virtual_machine" "mgmt-vm-instance" {
     type = var.vm_instance_identity_type
   }
 
-  dynamic "plan" {
-    for_each = local.custom_image_condition ? [
-    ] : [1]
-    content {
+  plan {
       name = var.vm_os_sku
       publisher = var.publisher
       product = var.vm_os_offer
     }
-  }
 
   boot_diagnostics {
     enabled = var.boot_diagnostics
@@ -117,25 +128,15 @@ resource "azurerm_virtual_machine" "mgmt-vm-instance" {
   }
 
   os_profile {
-    computer_name = var.mgmt_name
+    computer_name = var.nginx_name
     admin_username = var.admin_username
     admin_password = var.admin_password
-    custom_data = templatefile("${path.module}/cloud-init.sh", {
-      installation_type = var.installation_type
-      allow_upload_download = var.allow_upload_download
-      os_version = var.os_version
-      template_name = var.template_name
-      template_version = var.template_version
-      is_blink = var.is_blink
-      bootstrap_script64 = base64encode(var.bootstrap_script)
-      location = var.location
-      management_GUI_client_network = var.management_GUI_client_network
-    })
   }
 
   os_profile_linux_config {
     disable_password_authentication = local.SSH_authentication_type_condition
 
+    
     dynamic "ssh_keys" {
       for_each = local.SSH_authentication_type_condition ? [
         1] : []
@@ -147,15 +148,14 @@ resource "azurerm_virtual_machine" "mgmt-vm-instance" {
   }
 
   storage_image_reference {
-    id = local.custom_image_condition ? azurerm_image.custom-image[0].id : null
-    publisher = local.custom_image_condition ? null : var.publisher
+    publisher = var.publisher
     offer = var.vm_os_offer
     sku = var.vm_os_sku
     version = var.vm_os_version
   }
 
   storage_os_disk {
-    name = var.mgmt_name
+    name = var.nginx_name
     create_option = var.storage_os_disk_create_option
     caching = var.storage_os_disk_caching
     managed_disk_type = var.storage_account_type
--- nginx/variables.tf
+++ nginx/variables.tf
@@ -1,5 +1,5 @@
 //********************** Basic Configuration Variables **************************//
-variable "mgmt_name" {
+variable "nginx_name" {
   description = "Management name"
   type = string
 }
@@ -15,15 +15,10 @@ variable "location" {
 }
 
 //********************** Virtual Machine Instances Variables **************************//
-variable "source_image_vhd_uri" {
-  type = string
-  description = "The URI of the blob containing the development image. Please use noCustomUri if you want to use marketplace images."
-  default = "noCustomUri"
-}
 
 variable "admin_username" {
   description = "Administrator username of deployed VM. Due to Azure limitations 'notused' name can be used"
-  default = "notused"
+  default = "vm-admin"
 }
 
 variable "admin_password" {
@@ -49,32 +44,6 @@ locals { // locals for 'authentication_type' allowed values
   // will fail if [var.authentication_type] is invalid:
   validate_authentication_type_value = index(local.authentication_type_allowed_values, var.authentication_type)
 }
-variable "template_name" {
-  description = "Template name. Should be defined according to deployment type(mgmt, ha, vmss)"
-  type = string
-  default = "mgmt_terraform"
-}
-
-variable "template_version" {
-  description = "Template version. It is reccomended to always use the latest template version"
-  type = string
-  default = "20210111"
-}
-
-variable "installation_type" {
-  description = "Installaiton type"
-  type = string
-  default = "management"
-}
-
-locals { // locals for 'installation_type' allowed values
-  installation_type_allowed_values = [
-    "custom",
-    "management"
-  ]
-  // will fail if [var.installation_type] is invalid:
-  validate_installation_type_value = index(local.installation_type_allowed_values, var.installation_type)
-}
 
 variable "vm_size" {
   description = "Specifies size of Virtual Machine"
@@ -92,57 +61,22 @@ variable "vm_os_version" {
   default = "latest"
 }
 
-variable "os_version" {
-  description = "GAIA OS version"
-  type = string
-}
-
 variable "vm_os_offer" {
   description = "The name of the image offer to be deployed.Choose from: check-point-cg-r8030, check-point-cg-r8040, check-point-cg-r81"
   type = string
+  default = "nginx-14-05-2021"
 }
 
 variable "vm_os_sku" {
-  /*
-    Choose from:
-      - "sg-byol"
-      - "sg-ngtp-v2" (for R80.30 only)
-      - "sg-ngtx-v2" (for R80.30 only)
-      - "sg-ngtp" (for R80.40 and above)
-      - "sg-ngtx" (for R80.40 and above)
-      - "mgmt-byol"
-      - "mgmt-25"
-  */
   description = "The sku of the image to be deployed"
   type = string
+  default = "nginx"
 }
 
-locals { // locals for 'vm_os_sku' allowed values
-  vm_os_sku_allowed_values = [
-    "sg-byol",
-    "sg-ngtp",
-    "sg-ngtx",
-    "sg-ngtp-v2",
-    "sg-ngtx-v2",
-    "mgmt-byol",
-    "mgmt-25"
-  ]
-  // will fail if [var.vm_os_sku] is invalid:
-  validate_vm_os_sku_value = index(local.vm_os_sku_allowed_values, var.vm_os_sku)
-}
 
 variable "publisher" {
   description = "CheckPoint publicher"
-  default = "checkpoint"
-}
-
-variable "allow_upload_download" {
-  description = "Automatically download Blade Contracts and other important data. Improve product experience by sending data to Check Point"
-  type = bool
-}
-
-variable "is_blink" {
-  description = "Define if blink image is used for deployment"
+  default = "nilespartnersinc1617691698386"
 }
 
 variable "storage_account_tier" {
@@ -242,29 +176,13 @@ variable "vnet_allocation_method" {
   default = "Static"
 }
 
-variable "management_GUI_client_network" {
-  description = "Allowed GUI clients - GUI clients network CIDR"
-  type = string
-}
 
 locals {
-  regex_valid_management_GUI_client_network = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(/(3[0-2]|2[0-9]|1[0-9]|[0-9]))$"
-  // Will fail if var.management_GUI_client_network is invalid
-  regex_management_GUI_client_network = regex(local.regex_valid_management_GUI_client_network, var.management_GUI_client_network) == var.management_GUI_client_network ? 0 : "Variable [management_GUI_client_network] must be a valid IPv4 network CIDR."
-
   regex_valid_network_cidr = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(/(3[0-2]|2[0-9]|1[0-9]|[0-9]))|$"
   // Will fail if var.address_space is invalid
   # regex_address_space = regex(local.regex_valid_network_cidr, var.address_space) == var.address_space ? 0 : "Variable [address_space] must be a valid address in CIDR notation."
   // Will fail if var.subnet_prefix is invalid
   regex_subnet_prefix = regex(local.regex_valid_network_cidr, var.subnet_prefix) == var.subnet_prefix ? 0 : "Variable [subnet_prefix] must be a valid address in CIDR notation."
-}
-
-variable "bootstrap_script" {
-  description = "An optional script to run on the initial boot"
-  default = ""
-  type = string
-  #example:
-  #"touch /home/admin/bootstrap.txt; echo 'hello_world' > /home/admin/bootstrap.txt"
 }
 
 variable "nsg_id" {
